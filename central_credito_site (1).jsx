ddc95ff143b307e8528d78ab62d2b5055e0a2704
byuhh <ib.bruuh@gmail.com>
2025-09-26 21:54:08 -0300

    Update central_credito_site (1).jsx

--- central_credito_site (1).jsx
+++ central_credito_site (1).jsx
@@ -1,4 +1,7 @@
-import { Card, CardContent } from "@/components/ui/card";
+git remoto adicionar origem https://github.com/byuhh/central-do-credito.git
+ git branch -M principal 
+git push -u origem principal
+  import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
